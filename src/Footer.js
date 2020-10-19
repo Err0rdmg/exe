21a173d30b05f8ac9750144af93fd8d3422ef4de
Err0rdmg <73097364+Err0rdmg@users.noreply.github.com>
2020-10-19 12:27:37 +0530

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,1 @@
-import React from 'react';
-import './styles/footer.css';
 
-const Footer = () => {
-    return (
-        <div className="main-footer">
-            <div className="container">
-                <h3>  Made with <span role="img" aria-label="Red heart" style="color:#ff1f6d">❤️</span> by Moaksh kakkar</h3>
-            </div>
-        </div>
-    )
-}
-
-
-export default Footer;
